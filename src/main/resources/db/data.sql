211e58a1af153cba30f8e79a85acb229c0589d05
coderyu5523 <coderyu5523@naver.com>
2025-11-01 19:54:31 +0900

    chap02

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+insert into user_tb (username,password,email,created_at) values ('ssar','1234','ssar@metacoding.com',now());
+insert into user_tb (username,password,email,created_at) values ('cos','1234','cos@metacoding.com',now());
+
+insert into board_tb (title, content, created_at,user_id) values ('title1', 'content1', now(),1); 
+insert into board_tb (title, content, created_at,user_id) values ('title2', 'content2', now(),1); 
